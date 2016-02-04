5504f573c5473a441b2c8fd90739ed98d3d84b33
Kunihiko Miyanaga <miyanaga@gmail.com>
2016-02-05 03:56:22 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# このリポジトリは
+
+Ranklet [ランクレット] ( https://ranklet.com )用のテンプレートセットです。
+
+テンプレート編集画面でGithub上のパス ( 例: ideamans/ranklet-templates ) を指定することで簡単にインポートすることができます。
+
+このリポジトリをフォークして、カスタムテンプレートの制作・配布にお役立てください。
+
+# ファイル構成
+
+Ranklet [ランクレット] のテンプレートと同様にMustacheを利用できます。
+
+* ranking.html HTMLテンプレート
+* ranking.scss SCSSテンプレート
+* ranking.js JavaScriptテンプレート
+* README.md このファイル
+* config.yml 設定ファイル(未使用/拡張性のため確保)
---
+++ ranking.js
@@ -0,0 +1,11 @@
+// Hosted on GitHub
+document.addEventListener('DOMContentLoaded', function() {
+    var head = (document.getElementsByTagName('head'))[0],
+        target = document.getElementById("ranklet-{{context.id}}");
+    // {{#context.files.css.js}}
+    if ( head ) { head.innerHTML += '<style id="ranklet-style-{{id}}">{{{context.files.css.js}}}</style>'; }
+    // {{/context.files.css.js}}
+    // {{#context.files.html.js}}
+    if ( target ) { target.innerHTML = '{{{context.files.html.js}}}'; }
+    // {{/context.files.html.js}}
+});
